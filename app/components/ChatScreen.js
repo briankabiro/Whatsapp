8d1aa5951f1eb9b0a351c4604a5bd46b8aa2d0d9
Brian Kabiro <briankigondu@gmail.com>
2017-03-15 20:21:25 +0300

    Styled Chatscreen

--- app/components/ChatScreen.js
+++ app/components/ChatScreen.js
@@ -8,31 +8,89 @@ import {
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 var {height, width} = Dimensions.get('window');
-import {GiftedChat} from 'react-native-gifted-chat';
 
 /*add Text Input, add background
 	add send button, add photo and name of person in navbar, add keyboard
 */
+const ds = new ListView.DataSource({rowHasChanged:(r1,r2) => r1 !== r2});
+const convo = [];
 export default class ChatScreen extends Component{
 	constructor(props) {
 	  super(props);
 	  static navigationOptions = {
 	  	title:({ state }) => 'Chat with ${state.params.user}'
 	  };
-	  this.state = {messages: []};
-	  this.onSend = this.onSend.bind(this);
+	  this.state = {
+	  	datasource: ds.cloneWithRows(convo),
+	  	note:""
+	  }
 	}
+
+	eachMessage(x, image){
+		if(x.person == 2){
+			return (
+				<View>
+					<View>
+						<Text>{x.note}</Text>
+					</View>
+				</View>
+			)
+		} else{
+			return (
+				<View>
+					<View>
+						<Text>{x.note}</Text>
+					</View>
+				</View>
+			)
+		}
+	}
+
+	submitThis(){
+		convo.push({
+			person:1,
+			note: this.state.note
+		});
+		this.setState({
+			datasource: ds.cloneWithRows(convo.reverse()),
+			note:''
+		})
+		setTimeout(() => {
+			this.simulator();
+		}, 2000);
+	}
+
+	simulator(){
+		convo.reverse();
+		convo.push({person:2, note:"Achanga za ovyo mzito"})
+		this.setState({
+			datasource:ds.cloneWithRows(convo.reverse());
+		})
+	}
+
 	render(){
+		const {note} = this.state;
 		return(
-			<View style={styles.container}>
-				<Image source = {require('../assets/background.jpg')} style={styles.backgroundImage}>
+			<Image source = {require('../assets/background.jpg')} style={styles.container}>
+				<ListView 
+					enableEmptySections = {true}
+					renderScrollComponent = {props => <InvertibleScrollView {...props} inverted />}
+					noScroll = {true}
+					style={{flex:1}}
+					contentContainerStyle={{ justifyContent:'flex-end'}}
+					dataSource = {this.state.datasource}
+					renderRow = {(rowData) => this.eachMessage(rowData)}
+				/>
 				<View style={styles.textInput}>
 				<TextInput 
-
+					style={{flex:1}}
+					value={note}
+					onChangeText={(note) => this.setState({ note})}
+					onSubmitEditing ={() => this.submitThis()}
+					placeholder = "Enter Your Message here"
 				/>	
 				</View>
-				</Image>
-			</View>
+			</Image>
 		);
 	}
 }
@@ -41,10 +99,8 @@ export default class ChatScreen extends Component{
 const styles = StyleSheet.create({
 	container:{
 		flex:1,
-	},
-	backgroundImage:{
-		flexDirection:'row',
-		flex:1
+		width:null,
+		height:null
 	},
 	textInput:{
 		alignSelf:'flex-end',
